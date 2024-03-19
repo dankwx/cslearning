0291bcb8ca694140b487ea103acb00c5110b985e
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-19 02:46:57 -0300

    Add project files.

---
+++ cslearning/hello.cs
@@ -0,0 +1,16 @@
+﻿string name = "Daniel Kondlatsch";
+
+
+void WelcomeMessage()
+{
+    Console.WriteLine("******************");
+    Console.WriteLine(@"
+    |\__/,|   (`\
+  _.|o o  |_   ) )
+-(((---(((--------
+");
+    Console.WriteLine("******************");
+}
+
+
+WelcomeMessage();
