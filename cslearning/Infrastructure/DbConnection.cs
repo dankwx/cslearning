a10650e86d1ea9b3e95a3ecaeac2b5772591cb6b
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-19 12:02:11 -0300

    feature: start implementation of postgresql

---
+++ cslearning/Infrastructure/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslearning.Infrastructure
+{
+    public class DbConnection : IDisposable
+    {
+        public NpgsqlConnection Connection { get; set; }
+
+        public DbConnection()
+        {
+            Connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=animeco;User Id=postgres;Password=root;");
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Connection.Dispose();
+        }
+    }
+}
